a0652f41da66fcd7a85f720ff5dbc0bcae26c9c6
joysonchen <joysonchen@qq.com>
2018-03-16 10:41:50 +0800

    Update RealmHelper.java
    add initRealm method

--- RealmHelper.java
+++ RealmHelper.java
@@ -28,6 +28,25 @@ public class RealmHelper {
         this.mRealm = realm;
 
     }
+    
+    public static Realm getRealm(Context context) {
+        byte[] key = new byte[64];
+        new SecureRandom().nextBytes(key);
+        Realm.init(context);
+        RealmMigration migration = new RealmMigration() {
+            @Override
+            public void migrate(DynamicRealm realm, long oldVersion, long newVersion) {
+
+            }
+        };
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name("realmdb.realm") //文件名
+                .schemaVersion(0) //版本号
+                .migration(migration)//数据库版本迁移（数据库升级，当数据库中某个表添加字段或者删除字段）
+                .deleteRealmIfMigrationNeeded()//声明版本冲突时自动删除原数据库(当调用了该方法时，上面的方法将失效)。
+                .build();//创建
+        return Realm.getInstance(config);
+    }
 
     /**
      * 获取RealmOperation的单例
